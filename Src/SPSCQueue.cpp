bd0341ecfca9fa328238c04a3403d917ae228bc1
Togotogo98 <rimpaipb2014@gmail.com>
2026-07-21 21:56:32 +0100

    Phase 1: Ring buffer implementation

--- Src/SPSCQueue.cpp
+++ Src/SPSCQueue.cpp
@@ -1,11 +1,18 @@
 #include "../Include/SPSCQueue.h"
-
+#include <stdexcept>
+#define MIN_CAPACITY 2
 
 SPSCQueue::SPSCQueue(size_t capacity) :
     buffer(capacity),
     head(0),
     tail(0),
-    capacity(capacity) {}
+    capacity(capacity) 
+{
+    if (capacity < MIN_CAPACITY)
+    {
+        throw std::invalid_argument("Queue capacity must be at least 2.");
+    }
+}
 
 bool SPSCQueue::Empty() const
 {
